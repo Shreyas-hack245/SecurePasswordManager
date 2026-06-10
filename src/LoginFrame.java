2a790092a0660cd3dc90bb5b32f2f62e5987f144
Shreyas-hack245 <19776shreyasbhat@gmail.com>
2026-06-10 20:08:48 +0530

    Implemented initial LoginFrame UI for the Password Manager

---
+++ src/LoginFrame.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginFrame extends JFrame {
+
+    JLabel lblTitle, lblUsername, lblPassword;
+    JTextField txtUsername;
+    JPasswordField txtPassword;
+    JButton btnLogin, btnRegister;
+
+    public LoginFrame() {
+
+        setTitle("Secure Password Manager");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        setLayout(new GridLayout(5, 2, 10, 10));
+
+        lblTitle = new JLabel("PASSWORD MANAGER");
+        lblUsername = new JLabel("Username:");
+        lblPassword = new JLabel("Password:");
+
+        txtUsername = new JTextField();
+        txtPassword = new JPasswordField();
+
+        btnLogin = new JButton("Login");
+        btnRegister = new JButton("Register");
+
+        add(lblTitle);
+        add(new JLabel(""));
+
+        add(lblUsername);
+        add(txtUsername);
+
+        add(lblPassword);
+        add(txtPassword);
+
+        add(btnLogin);
+        add(btnRegister);
+
+        setVisible(true);
+    }
+}
