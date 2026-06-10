d71439f34fd97a86663fbc5f0ccb8d0f93462974
Shreyas-hack245 <19776shreyasbhat@gmail.com>
2026-06-10 20:35:47 +0530

    Update database password constant to use lowercase for consistency

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection {
 
     private static final String USER = "root";
 
-    private static final String PASSWORD = "YOUR_PASSWORD";
+    private static final String PASSWORD = "your password";
 
     public static Connection getConnection() {
 
