6a10160b4c5305e8580d8a333e92eab60bcc2de4
Shreyas-hack245 <19776shreyasbhat@gmail.com>
2026-06-10 20:06:19 +0530

    Added database connection

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/password_manager";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "your_password";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
